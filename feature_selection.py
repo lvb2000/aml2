195d3d49475888be67309b8e19df95b79dbce02c
Lukas von Briel <lvonbriel@student.ethz.ch>
2022-11-18 20:33:50 +0100

    basic heartbeat extractor + plot

--- feature_selection.py
+++ feature_selection.py
@@ -1,16 +1,49 @@
 import pandas as pd
 import numpy as np
 import biosppy.signals.ecg as ecg
+import matplotlib.pyplot as plt
 
 class biosppy:
 
-    sampling_rate=1000
+    sampling_rate=300
 
     def __init__(self,sampling_rate):
         self.sampling_rate=sampling_rate
 
     def fit(self,X,show=False):
+        means= np.empty([1,1000])
+        variances = np.empty([1,1000])
+        count = 0
         for i in X:
-            signal = i.dropna()
-            ecg.ecg(signal=signal, sampling_rate=self.sampling_rate, show=show)
-        return X
+            count+=1
+            signal = X[i].dropna()
+
+            # extract heartbeats with ecg function
+            extracted_heartbeats = ecg.ecg(signal=signal, sampling_rate=self.sampling_rate, show=show)[4]
+
+            #compute mean and variance of extracted heartbeats
+            mean,variance = self.__mean_variance__(extracted_heartbeats)
+
+            #add them into array
+            mean=np.reshape(np.pad(mean, (0, 1000 - np.size(mean)), 'constant', constant_values=(np.nan, np.nan)),(1,1000))
+            variance=np.reshape(np.pad(variance, (0, 1000 - np.size(variance)), 'constant', constant_values=(np.nan, np.nan)),(1,1000))
+            means = np.concatenate((means,mean),axis=0)
+            variances = np.concatenate((variances,variance),axis=0)
+
+            if count==20:
+                return means,variances
+        return means,variances
+
+    def plot(self,mean,variance):
+        mean = mean[np.logical_not(np.isnan(mean))]
+        variance = variance[np.logical_not(np.isnan(variance))]
+        x=np.arange(0,np.size(mean)/self.sampling_rate,1/self.sampling_rate)
+        plt.plot(x, mean, '-', color='gray')
+        plt.fill_between(x, mean - variance, mean + variance,color='gray', alpha=0.2)
+        plt.show()
+        return
+    def __mean_variance__(self,out):
+        mean = np.mean(out,axis=0)
+        variance = np.std(out,axis=0)
+        return mean , variance
+
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
 import feature_selection as fs
+
 
 def get_data():
     x_train_ = pd.read_csv('X_train.csv').drop('id', axis=1)
@@ -10,13 +10,19 @@ def get_data():
     x_test_ = pd.read_csv('X_test.csv').drop('id', axis=1)
     return x_train_, y_train_, x_test_
 
+x_train_, y_train_, x_test_ = get_data()
+
 def make_submission(prediction_, name='submission.csv'):
     dt = pd.DataFrame(data=prediction_, columns=['y'])
     dt['id'] = dt.index
     dt = dt[['id', 'y']]
     dt.to_csv(name, header=True, index=False)
 
-x_train_, y_train_, x_test_ = get_data()
-
 signals = fs.biosppy(sampling_rate=1000)
-x_train_ = signals.fit(x_train_,True)
+means,variances = signals.fit(x_train_)
+signals.plot(means[1,:],variances[1,:])
+signals.plot(means[2,:],variances[2,:])
+signals.plot(means[3,:],variances[3,:])
+signals.plot(means[4,:],variances[4,:])
+
+
