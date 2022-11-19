05069cc6cf3af104218e9ecb709d3cbd4a472000
Lukas von Briel <lvonbriel@student.ethz.ch>
2022-11-19 16:11:02 +0100

    feature selection fixed

--- feature_selection.py
+++ feature_selection.py
@@ -12,38 +12,40 @@ class biosppy:
 
     def fit(self,X,show=False):
         means= np.empty([1,1000])
-        variances = np.empty([1,1000])
-        count = 0
-        for i in X:
-            count+=1
-            signal = X[i].dropna()
+        stds = np.empty([1,1000])
+        for i,row in X.iterrows():
+            signal = row.dropna().to_numpy(dtype='float32')
 
-            # extract heartbeats with ecg function
-            extracted_heartbeats = ecg.ecg(signal=signal, sampling_rate=self.sampling_rate, show=show)[4]
+            r_peaks = ecg.engzee_segmenter(signal, self.sampling_rate)['rpeaks']
+            if len(r_peaks) >= 2:
+                extracted_heartbeats = ecg.extract_heartbeats(signal, r_peaks, self.sampling_rate)['templates']
 
-            #compute mean and variance of extracted heartbeats
-            mean,variance = self.__mean_variance__(extracted_heartbeats)
+                #compute mean and std of extracted heartbeats
+                mean,std = self.__mean_variance__(extracted_heartbeats)
 
-            #add them into array
-            mean=np.reshape(np.pad(mean, (0, 1000 - np.size(mean)), 'constant', constant_values=(np.nan, np.nan)),(1,1000))
-            variance=np.reshape(np.pad(variance, (0, 1000 - np.size(variance)), 'constant', constant_values=(np.nan, np.nan)),(1,1000))
-            means = np.concatenate((means,mean),axis=0)
-            variances = np.concatenate((variances,variance),axis=0)
+                #add them into array
+                mean=np.reshape(np.pad(mean, (0, 1000 - np.size(mean)), 'constant', constant_values=(np.nan, np.nan)),(1,1000))
+                std=np.reshape(np.pad(std, (0, 1000 - np.size(std)), 'constant', constant_values=(np.nan, np.nan)),(1,1000))
+                means = np.concatenate((means,mean),axis=0)
+                stds = np.concatenate((stds,std),axis=0)
 
-            if count==20:
-                return means,variances
-        return means,variances
+
+        means = np.delete(means, 0, 0)
+        stds = np.delete(stds,0,0)
+        return means,stds
 
     def plot(self,mean,variance):
         mean = mean[np.logical_not(np.isnan(mean))]
         variance = variance[np.logical_not(np.isnan(variance))]
-        x=np.arange(0,np.size(mean)/self.sampling_rate,1/self.sampling_rate)
-        plt.plot(x, mean, '-', color='gray')
-        plt.fill_between(x, mean - variance, mean + variance,color='gray', alpha=0.2)
+        plt.figure()
+        plt.plot(range(mean.shape[0]), mean, '-', color='gray')
+        plt.fill_between(range(mean.shape[0]), mean - variance, mean + variance,color='gray', alpha=0.2)
+        plt.xlabel('Time [s]')
+        plt.ylabel('Amplitude [mV]')
         plt.show()
         return
     def __mean_variance__(self,out):
         mean = np.mean(out,axis=0)
-        variance = np.std(out,axis=0)
-        return mean , variance
+        std = np.std(out,axis=0)
+        return mean , std
 
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import matplotlib.pyplot as plt
+## imports
 import numpy as np
 import pandas as pd
 import feature_selection as fs
+import matplotlib.pyplot as plt
 
-
+## import data
 def get_data():
     x_train_ = pd.read_csv('X_train.csv').drop('id', axis=1)
     y_train_ = pd.read_csv('y_train.csv').drop('id', axis=1)
@@ -12,17 +13,58 @@ def get_data():
 
 x_train_, y_train_, x_test_ = get_data()
 
-def make_submission(prediction_, name='submission.csv'):
-    dt = pd.DataFrame(data=prediction_, columns=['y'])
-    dt['id'] = dt.index
-    dt = dt[['id', 'y']]
-    dt.to_csv(name, header=True, index=False)
+## Plot sample signal for each class
 
-signals = fs.biosppy(sampling_rate=1000)
-means,variances = signals.fit(x_train_)
-signals.plot(means[1,:],variances[1,:])
-signals.plot(means[2,:],variances[2,:])
-signals.plot(means[3,:],variances[3,:])
-signals.plot(means[4,:],variances[4,:])
+def plot_sample_signals_for_each_class(data,labels):
+    labels_array = labels['y'].to_numpy()
+    num_classes = 4
+    sample_signal_ids = []
+    for class_id in range(num_classes):
+        sample_signal_ids.append(int(np.argwhere(labels_array == class_id)[0]))
 
+    # Some matplotlib setting
+    plt.rcParams["figure.figsize"] = (30, 20)
+    plt.rcParams['lines.linewidth'] = 5
+    plt.rcParams['xtick.labelsize'] = 24
+    plt.rcParams['ytick.labelsize'] = 32
+    plt.rcParams['axes.labelsize'] = 48
+    plt.rcParams['axes.titlesize'] = 48
 
+    fig, axs = plt.subplots(4, 1)
+
+    seconds = np.arange(0, 600) / 30
+    x_labels = [0, 5, 10, 15, 20]
+
+    for class_id in range(num_classes):
+        ax = axs[class_id]
+        # ax.set_title("Class {}".format(class_id))
+
+        measurements = data.loc[sample_signal_ids[class_id]].dropna().to_numpy(dtype='float32')
+        # Get a subsequence of a signal and downsample it for visualization purposes
+        measurements = measurements[1000:7000:10]
+        # convert volts to millivolts
+        measurements /= 1000
+        ax.plot(seconds, measurements, color='k')
+        ax.set_xticks(x_labels)
+
+    # Display x- and y-labels for the whole plot
+    ax = fig.add_subplot(111, frameon=False)
+    # hide tick and tick label of the big axes
+    plt.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
+    ax.yaxis.set_label_coords(-0.05, 0.5)
+    plt.xlabel('Time [s]')
+    plt.ylabel('Amplitude [mV]')
+    plt.show()
+
+plot_sample_signals_for_each_class(x_train_,y_train_)
+
+## extract features
+
+signals = fs.biosppy(sampling_rate=300)
+means, stds = signals.fit(x_train_,show=False)
+signals.plot(means[0,:], stds[0, :])
+signals.plot(means[1,:], stds[1, :])
+signals.plot(means[2,:], stds[2, :])
+signals.plot(means[3,:], stds[3, :])
+
+print(y_train_[0:4])
