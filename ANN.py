77ed4d6d31fcdff1789ae32324bc62d8c823decf
Lukas von Briel <lvonbriel@student.ethz.ch>
2022-12-03 16:01:13 +0100

    MLP tuned for 0.77 acc

--- ANN.py
+++ ANN.py
@@ -9,8 +9,9 @@ class MultiLayerPerceptron(torch.nn.Module):
         super().__init__()
 
         # attach different layers/activations to self
-        self.fc1 = torch.nn.Linear(input_dim,20)
-        self.fc_out = torch.nn.Linear(20,output_dim)
+        self.fc1 = torch.nn.Linear(input_dim,120)
+        self.fc2 = torch.nn.Linear(input_dim, 120)
+        self.fc_out = torch.nn.Linear(120,output_dim)
         self.activation_fn = torch.nn.Tanh()
         self.activation_fn1 = torch.nn.LogSigmoid()
 
@@ -18,114 +19,162 @@ class MultiLayerPerceptron(torch.nn.Module):
         result=self.fc1(x)
         result=self.activation_fn(result)
         result=self.activation_fn1(result)
+        #result=self.fc2(x)
+        #result=self.activation_fn(result)
+        #result=self.activation_fn1(result)
         logits=self.fc_out(result)
         return logits
 
-def fit_MLP1(x_train,x_test,y_train,y_test):
+class MLP1():
 
-    #----- convert features and labels to tensors -----#
-    x_train_t = torch.from_numpy(x_train).float()
-    x_test_t = torch.from_numpy(x_test).float()
+    def __init__(self,num_classes:int,epochs:int,predict=False):
+        # define general parameters
+        self.num_classes = num_classes
+        self.epochs = epochs
+        self.predict = predict
 
-    y_train_t = torch.from_numpy(y_train).long()
-    y_test_t = torch.from_numpy(y_test).long()
+    def load_data(self,x_train,x_test,y_train,y_test):
+        # ----- convert features and labels to tensors -----#
+        x_train_t = torch.from_numpy(x_train).float()
+        y_train_t = torch.from_numpy(y_train).long()
 
-    #----- compute mean and std of training images -----#
-    x_std, x_mean = torch.std_mean(x_train_t)
+        if not self.predict:
+            x_test_t = torch.from_numpy(x_test).float()
+            y_test_t = torch.from_numpy(y_test).long()
 
-    #----- standardize images -----#
-    x_train_t = (x_train_t - x_mean) / (x_std + 1e-6)
-    x_test_t = (x_test_t - x_mean) / (x_std + 1e-6)
+        # ----- compute mean and std of training images -----#
+        x_std, x_mean = torch.std_mean(x_train_t)
 
-    #----- convert lables to one-hot encoding -----#
-    y_train_t = torch.nn.functional.one_hot(y_train_t, num_classes=4).float()
-    y_test_t = torch.nn.functional.one_hot(y_test_t, num_classes=4).float()
+        # ----- standardize-----#
+        x_train_t = (x_train_t - x_mean) / (x_std + 1e-6)
+        if not self.predict:
+            x_test_t = (x_test_t - x_mean) / (x_std + 1e-6)
 
-    #----- setup dataset -----#
-    train_ds = TensorDataset(x_train_t, y_train_t)
-    test_ds = TensorDataset(x_test_t, y_test_t)
+        # ----- convert lables to one-hot encoding -----#
+        y_train_t = torch.nn.functional.one_hot(y_train_t, num_classes=self.num_classes).float()
+        if not self.predict:
+            y_test_t = torch.nn.functional.one_hot(y_test_t, num_classes=self.num_classes).float()
 
-    #----- setup a Weighted Random Sampler to account for Lable imbalance -----#
-    class_counts = [np.size(np.where(y_train==0)),np.size(np.where(y_train==1))]
-    num_samples = sum(class_counts)
-    class_weights = [num_samples/class_counts[i] for i in range(len(class_counts))]
-    weights = [class_weights[y_train[i]] for i in range(int(num_samples))]
-    sampler = WeightedRandomSampler(weights=torch.DoubleTensor(weights),num_samples=int(num_samples), replacement=True)
+        # ----- setup dataset -----#
+        train_ds = TensorDataset(x_train_t, y_train_t)
+        if not self.predict:
+            test_ds = TensorDataset(x_test_t, y_test_t)
 
-    #----- setup train and test loader -----#
-    train_loader = DataLoader(train_ds, batch_size=128, sampler=sampler)
-    test_loader = DataLoader(test_ds, batch_size=512, shuffle=True)
+        # ----- setup a Weighted Random Sampler to account for Lable imbalance -----#
+        class_counts = 0
+        if self.num_classes == 2:
+            class_counts = [np.size(np.where(y_train == 0)), np.size(np.where(y_train == 1))]
+        elif self.num_classes == 3:
+            class_counts = [np.size(np.where(y_train == 0)), np.size(np.where(y_train == 1)),
+                            np.size(np.where(y_train == 2))]
+        elif self.num_classes == 4:
+            class_counts = [np.size(np.where(y_train == 0)), np.size(np.where(y_train == 1)),
+                            np.size(np.where(y_train == 2)), np.size(np.where(y_train == 3))]
+        else:
+            assert("Number of classes out of bounds")
 
-    #----- generate model -----#
-    model = MultiLayerPerceptron(x_train.shape[1],4)
+        num_samples = sum(class_counts)
+        class_weights = [num_samples / class_counts[i] for i in range(len(class_counts))]
+        weights = [class_weights[y_train[i]] for i in range(int(num_samples))]
+        sampler = WeightedRandomSampler(weights=torch.DoubleTensor(weights), num_samples=int(num_samples),
+                                        replacement=True)
 
-    #----- define loss function -----#
-    loss_fn = torch.nn.CrossEntropyLoss()
+        # ----- setup train and test loader -----#
+        self.train_loader = DataLoader(train_ds, batch_size=128, sampler=sampler)
+        if not self.predict:
+            self.test_loader = DataLoader(test_ds, batch_size=512, shuffle=True)
 
-    #----- Setup the optimizer (adaptive learning rate method) -----#
-    optim = torch.optim.Adam(model.parameters(), lr=1e-3)
+    def load_model(self,x_train):
+        # ----- generate model -----#
+        self.model = MultiLayerPerceptron(x_train.shape[1], self.num_classes)
 
-    #----- Perform training and testing -----#
+        # ----- define loss function -----#
+        self.loss_fn = torch.nn.CrossEntropyLoss()
 
-    for epoch in range(50):
-        # reset statistics trackers
-        train_loss_cum = 0.0
-        acc_cum = 0.0
-        num_samples_epoch = 0
-        t = time.time()
-
-        # Go once through the training dataset (-> epoch)
-        for x_batch, y_batch in train_loader:
+        # ----- Setup the optimizer (adaptive learning rate method) -----#
+        self.optim = torch.optim.Adam(self.model.parameters(), lr=1e-3)
+    def fit_one_epoch(self):
+        for x_batch, y_batch in self.train_loader:
             # zero grads and put model into train mode
-            optim.zero_grad()
-            model.train()
+            self.optim.zero_grad()
+            self.model.train()
 
             # forward pass
-            logits = model(x_batch)
-            loss = loss_fn(logits, y_batch)
+            logits = self.model(x_batch)
+            loss = self.loss_fn(logits, y_batch)
 
             # backward pass and gradient step
             loss.backward()
-            optim.step()
+            self.optim.step()
 
             # keep track of train stats
             num_samples_batch = x_batch.shape[0]
-            num_samples_epoch += num_samples_batch
-            train_loss_cum += loss * num_samples_batch
-            acc_cum += accuracy(logits, y_batch) * num_samples_batch
+            self.num_samples_epoch += num_samples_batch
+            self.train_loss_cum += loss * num_samples_batch
+            self.acc_cum += self.accuracy(logits, y_batch) * num_samples_batch
 
-        # average the accumulated statistics
-        avg_train_loss = train_loss_cum / num_samples_epoch
-        avg_acc = acc_cum / num_samples_epoch
-        test_acc = evaluate(model,test_loader)
-        epoch_duration = time.time() - t
+        return
 
-        # print some infos
-        print(f'Epoch {epoch} | Train loss: {train_loss_cum.item():.4f} | '
-              f' Train accuracy: {avg_acc.item():.4f} | Test accuracy: {test_acc.item():.4f} |'
-              f' Duration {epoch_duration:.2f} sec')
+    def train(self,x_train,y_train,x_test=0,y_test=0):
 
-    return 0
+        self.load_data(x_train,x_test,y_train,y_test)
+
+        self.load_model(x_train)
+
+        for epoch in range(self.epochs):
+            # reset statistics trackers
+            self.train_loss_cum = 0.0
+            self.acc_cum = 0.0
+            self.num_samples_epoch = 0
+            t = time.time()
+
+            self.fit_one_epoch()
+
+            # average the accumulated statistics
+            avg_train_loss = self.train_loss_cum / self.num_samples_epoch
+            avg_acc = self.acc_cum / self.num_samples_epoch
+            test_acc = torch.tensor(0.0)
+            if not self.predict:
+                test_acc = self.evaluate(self.model, self.test_loader)
+            epoch_duration = time.time() - t
+
+            # print some infos
+            print(f'Epoch {epoch} | Train loss: {self.train_loss_cum.item():.4f} | '
+                  f' Train accuracy: {avg_acc.item():.4f} | Test accuracy: {test_acc.item():.4f} |'
+                  f' Duration {epoch_duration:.2f} sec')
+
+    def accuracy(self,logits: torch.Tensor, label: torch.tensor) -> torch.Tensor:
+        # computes the classification accuracy
+        correct_label = torch.argmax(logits, axis=-1) == torch.argmax(label, axis=-1)
+        assert correct_label.shape == (logits.shape[0],)
+        acc = torch.mean(correct_label.float())
+        assert 0. <= acc <= 1.
+        return acc
+
+    def evaluate(self,model: torch.nn.Module, test_loader) -> torch.Tensor:
+        # goes through the test dataset and computes the test accuracy
+        model.eval()  # bring the model into eval mode
+        with torch.no_grad():
+            acc_cum = 0.0
+            num_eval_samples = 0
+            for x_batch_test, y_label_test in test_loader:
+                batch_size = x_batch_test.shape[0]
+                num_eval_samples += batch_size
+                acc_cum += self.accuracy(model(x_batch_test), y_label_test) * batch_size
+            avg_acc = acc_cum / num_eval_samples
+            assert 0 <= avg_acc <= 1
+            return avg_acc
+
+    def predict_test_set(self,x_test):
+        if not self.predict:
+            self.prediction = 0
+
+        x_test_t = torch.from_numpy(x_test).float()
+        x_std, x_mean = torch.std_mean(x_test_t)
+        x_test_t = (x_test_t - x_mean) / (x_std + 1e-6)
+
+        self.model.eval()
+        with torch.no_grad():
+            self.prediction = torch.argmax(self.model(x_test_t), axis=-1).numpy()
 
 
-def accuracy(logits:torch.Tensor,label:torch.tensor) -> torch.Tensor:
-    # computes the classification accuracy
-    correct_label = torch.argmax(logits, axis=-1) == torch.argmax(label, axis=-1)
-    assert correct_label.shape == (logits.shape[0],)
-    acc = torch.mean(correct_label.float())
-    assert 0. <= acc <= 1.
-    return acc
-
-def evaluate(model: torch.nn.Module,test_loader) -> torch.Tensor:
-  # goes through the test dataset and computes the test accuracy
-  model.eval()  # bring the model into eval mode
-  with torch.no_grad():
-    acc_cum = 0.0
-    num_eval_samples = 0
-    for x_batch_test, y_label_test in test_loader:
-      batch_size = x_batch_test.shape[0]
-      num_eval_samples += batch_size
-      acc_cum += accuracy(model(x_batch_test), y_label_test) * batch_size
-    avg_acc = acc_cum / num_eval_samples
-    assert 0 <= avg_acc <= 1
-    return avg_acc
--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import feature_creation as fc
 import matplotlib.pyplot as plt
 import ANN
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
+
 
 #----------- IMPORT RAW ECG SIGNALS -----------#
 """
@@ -23,30 +25,46 @@ signals.toCSV(y_train1)
 
 
 #----------- IMPORT EXTRACTED HEARTBEATS FROM CSV -----------#
-
+"""
 mean_train = pd.read_csv('mean')
 std_train = pd.read_csv('std')
 y_train1 = pd.read_csv('y_train1')
-
+"""
 #----------- CREATE FEATURES FROM HEARTBEATS -----------#
-
+"""
 features = fc.feature_creation()
 features.createFeatures(std_train.to_numpy(),mean_train.to_numpy())
 df = features.getFeatures()
-
+"""
 #----------- PLOT FEATURES -----------#
-
+"""
 features.plotMeanStd(y_train1,mean_train,std_train)
+"""
+
+#----------- IMPORT FEATURES -----------#
+
+x_train = pd.read_csv('features_ata.csv')
+x_test = pd.read_csv('test_features_ata.csv')
+y_train = pd.read_csv('y_train.csv')
+x_train.drop(x_train.columns[0],axis=1,inplace=True)
+x_test.drop(x_test.columns[0],axis=1,inplace=True)
+y_train.drop(y_train.columns[0],axis=1,inplace=True)
+x_train=x_train.to_numpy()
+y_train=y_train.to_numpy()
 
 #----------- TRAIN ANN CLASSIFYING 0,1,2 FROM 3 -----------#
 
-y_train_combined = y_train1.to_numpy()
-y_train_combined[y_train_combined == 1] = 0
-y_train_combined[y_train_combined == 2] = 0
-y_train_combined[y_train_combined == 3] = 1
+#y_train_combined = y_train1.to_numpy()
+#y_train_combined[y_train_combined == 1] = 0
+#y_train_combined[y_train_combined == 2] = 0
+#y_train_combined[y_train_combined == 3] = 1
 
-x_train, x_test_val, y_train, y_test_val = train_test_split(df.to_numpy(), y_train_combined, test_size=0.4, random_state=42)
-ANN.fit_MLP1(x_train,x_test_val,np.squeeze(y_train),np.squeeze(y_test_val))
+#x_train, x_test_val, y_train, y_test_val = train_test_split(x_train, y_train, test_size=0.4, random_state=42)
+model = ANN.MLP1(num_classes=4,epochs=420,predict=True)
+model.train(x_train,np.squeeze(y_train))
+#,x_test_val,np.squeeze(y_test_val)
+model.predict_test_set(x_test.to_numpy())
+
 
 #----------- TRAIN SVC CLASSIFYING 0,1,2 FROM 3 -----------#
 
@@ -54,3 +72,10 @@ ANN.fit_MLP1(x_train,x_test_val,np.squeeze(y_train),np.squeeze(y_test_val))
 #----------- TRAIN KNN CLASSIFYING 0,1,2 FROM 3 -----------#
 
 #----------- COMBINE ALL CLASSIFIERS ---------#
+
+#----------- MAKE SUBMISSION ---------#
+
+dt = pd.DataFrame(data=model.prediction, columns=['y'])
+dt['id'] = dt.index
+dt = dt[['id', 'y']]
+dt.to_csv('submission', header=True, index=False)
